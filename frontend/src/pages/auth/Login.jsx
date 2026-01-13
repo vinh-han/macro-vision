a9f997b5acffa350f672f317260e6c6cfc7d3578
anle404 <anle442004@gmail.com>
2026-01-13 13:13:16 +0700

    feat(frontend): update auth page, add constant file

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,9 +1,20 @@
 import { Box, Text, Field, Input, Button } from '@chakra-ui/react'
 import { useState, useRef } from 'react'
 import { useNavigate } from 'react-router'
+import { BASE_API_URL } from '../../constant';
 
 export default function Login() {
     let navigate = useNavigate();
+    const [errorMsg, setErrorMsg] = useState("")
+
+    function onSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData.entries());
+
+        console.log(data);
+    }
+
     return (
         <Box
             width="inherit"
@@ -24,56 +35,62 @@ export default function Login() {
                     textAlign="center" 
                     fontWeight="semibold" 
                     fontSize="2.1rem">Welcome Back!</Text>
-                <Box
-                    padding="1.3rem"
-                    display="flex"
-                    flexDirection="column"
-                    gap="3rem"
-                    bgColor="crimsonred.600"  
-                    rounded="0.8rem"
-                    boxSizing="border-box"
-                    fontSize="1rem">
-                    <Box>
-                        <Field.Root required invalid>
-                            <Field.Label fontSize="1.15em" fontWeight="semibold">
-                                Username <Field.RequiredIndicator />
-                            </Field.Label>
-                            <Input 
-                                marginTop="0.1rem" 
-                                placeholder="username123" 
-                                variant="subtle" 
-                                rounded="0.5rem" 
-                                fontSize="1em" 
-                                color="black" />
-                            <Field.ErrorText fontWeight="bold" fontSize="1.2rem"></Field.ErrorText>
-                        </Field.Root>
-                        <Field.Root required marginTop="1.2rem">
-                            <Field.Label fontSize="1.15em" fontWeight="semibold">
-                                Password <Field.RequiredIndicator />
-                            </Field.Label>
-                            <Input 
-                                type="password" 
-                                marginTop="0.1rem" 
-                                placeholder="password123" 
-                                variant="subtle" 
-                                rounded="0.5rem" 
-                                fontSize="1em" 
-                                color="black"/>
-                        </Field.Root>
-                        <Text 
-                            marginTop="0.8rem" 
-                            textAlign="right" 
-                            textDecoration="underline">
-                            Forgot Password?
-                        </Text>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <Box
+                        padding="1.3rem"
+                        display="flex"
+                        flexDirection="column"
+                        gap="3rem"
+                        bgColor="crimsonred.600"  
+                        rounded="0.8rem"
+                        boxSizing="border-box"
+                        fontSize="1rem">
+                        <Box>
+                            <Field.Root required>
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Username <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input 
+                                    name="username"
+                                    placeholder="username123"
+                                    marginTop="0.1rem"  
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black" />
+                                <Field.ErrorText fontWeight="bold" fontSize="1.2rem"></Field.ErrorText>
+                            </Field.Root>
+                            <Field.Root required marginTop="1.2rem">
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Password <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input 
+                                    name="password"
+                                    type="password" 
+                                    placeholder="password123"
+                                    marginTop="0.1rem"  
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black"/>
+                            </Field.Root>
+                            <Text 
+                                marginTop="0.8rem" 
+                                textAlign="right" 
+                                textDecoration="underline">
+                                Forgot Password?
+                            </Text>
+                        </Box>
+                        <Button 
+                            type="submit"
+                            width="100%" 
+                            rounded="0.8rem" 
+                            fontSize="1.2rem"
+                            fontWeight="semibold">
+                            Log In
+                        </Button>
                     </Box>
-                    <Button 
-                        type="submit"
-                        width="100%" 
-                        rounded="0.8rem" 
-                        fontSize="1.2rem"
-                        fontWeight="semibold">Log In</Button>
-                </Box>
+                </form>
             </Box>
             <Text
                 textAlign="center" 
--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,9 +1,52 @@
-import { Box, Text, Field, Input, Button } from '@chakra-ui/react'
+import { Box, Text, Field, Input, Button, HStack } from '@chakra-ui/react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router'
+import { BASE_API_URL } from '../../constant';
 
 export default function Signup() {
-    
     let navigate = useNavigate();
+
+    const [errorMsg, setErrorMsg] = useState("")
+    
+    function onSubmit(e) {
+        e.preventDefault(e);
+
+        const formData = new FormData(e.currentTarget);
+        const data = Object.fromEntries(formData.entries());
+
+        if (data.password !== data.conf_password) {
+            setErrorMsg("Password Mismatch!")
+            return
+        }
+
+        const signUpInfo = {
+            username: data.username,
+            display_name: data.display_name,
+            email: data.email,
+            password: data.password
+        }
+
+        fetch(`${BASE_API_URL}auth/signup`, {
+            method: "POST",
+            body: JSON.stringify(signUpInfo),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            if (response.status === 201) {
+                return response.json()
+            }
+
+            return Promise.reject()
+        }).then((data) => {
+            console.log(data)
+        }).catch((response) => {
+            console.log(response)
+        })
+
+        setErrorMsg("")
+    }
+
     return (
         <Box
             width="inherit"
@@ -24,74 +67,106 @@ export default function Signup() {
                     textAlign="center" 
                     fontWeight="semibold" 
                     fontSize="2.1rem">Create Your Account</Text>
-                <Box
-                    padding="1.3rem"
-                    display="flex"
-                    flexDirection="column"
-                    gap="5rem"
-                    bgColor="crimsonred.600"  
-                    rounded="0.8rem"
-                    boxSizing="border-box"
-                    fontSize="1rem">
-                    <Box>
-                        <Field.Root required>
-                            <Field.Label fontSize="1.15em" fontWeight="semibold">
-                                Username <Field.RequiredIndicator />
-                            </Field.Label>
-                            <Input 
-                                marginTop="0.1rem" 
-                                placeholder="bunappetit123" 
-                                variant="subtle" 
-                                rounded="0.5rem" 
-                                fontSize="1em" 
-                                color="black" />
-                        </Field.Root>
-                        <Field.Root required marginTop="1.2rem">
-                            <Field.Label fontSize="1.15em" fontWeight="semibold">
-                                Full Name <Field.RequiredIndicator />
-                            </Field.Label>
-                            <Input 
-                                marginTop="0.1rem" 
-                                placeholder="Bun Appetit" 
-                                variant="subtle" 
-                                rounded="0.5rem" 
-                                fontSize="1em" 
-                                color="black" />
-                        </Field.Root>
-                        <Field.Root required marginTop="1.2rem">
-                            <Field.Label fontSize="1.15em" fontWeight="semibold">
-                                Email <Field.RequiredIndicator />
-                            </Field.Label>
-                            <Input
-                                type="email" 
-                                marginTop="0.1rem" 
-                                placeholder="bunappetit123@" 
-                                variant="subtle" 
-                                rounded="0.5rem" 
-                                fontSize="1em" 
-                                color="black" />
-                        </Field.Root>
-                        <Field.Root required marginTop="1.2rem">
-                            <Field.Label fontSize="1.15em" fontWeight="semibold">
-                                Password <Field.RequiredIndicator />
-                            </Field.Label>
-                            <Input 
-                                type="password" 
-                                marginTop="0.1rem" 
-                                placeholder="password123" 
-                                variant="subtle" 
-                                rounded="0.5rem" 
-                                fontSize="1em" 
-                                color="black"/>
-                        </Field.Root>
+                <form onSubmit={(e) => onSubmit(e)}>
+                    <Box
+                        padding="1.3rem"
+                        display="flex"
+                        flexDirection="column"
+                        gap="5rem"
+                        bgColor="crimsonred.600"  
+                        rounded="0.8rem"
+                        boxSizing="border-box"
+                        fontSize="1rem">
+                        <Box>
+                            <Field.Root required>
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Username <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input 
+                                    name="username"
+                                    placeholder="bunappetit123" 
+                                    marginTop="0.1rem" 
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black" />
+                            </Field.Root>
+                            <Field.Root required marginTop="1.2rem">
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Full Name <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input
+                                    name="display_name" 
+                                    placeholder="Bun Appetit"
+                                    marginTop="0.1rem"  
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black" />
+                            </Field.Root>
+                            <Field.Root required marginTop="1.2rem">
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Email <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input
+                                    name="email"
+                                    type="email" 
+                                    marginTop="0.1rem" 
+                                    placeholder="bunappetit123@" 
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black" />
+                            </Field.Root>
+                            <Field.Root required marginTop="1.2rem">
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Password <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input 
+                                    name="password"
+                                    type="password" 
+                                    marginTop="0.1rem" 
+                                    placeholder="password123" 
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black"/>
+                            </Field.Root>
+                            <Field.Root required marginTop="1.2rem">
+                                <Field.Label fontSize="1.15em" fontWeight="semibold">
+                                    Confirm Password <Field.RequiredIndicator />
+                                </Field.Label>
+                                <Input
+                                    name="conf_password" 
+                                    type="password"
+                                    marginTop="0.1rem" 
+                                    placeholder="password123" 
+                                    variant="subtle" 
+                                    rounded="0.5rem" 
+                                    fontSize="1em" 
+                                    color="black"/>
+                            </Field.Root>
+                            {errorMsg && (
+                                <HStack justifyContent="center" fontSize="1.2em" marginTop="1.4rem">
+                                    <i className="ri-error-warning-fill" style={{lineHeight: 1}}></i>
+                                    <Text 
+                                        textAlign="center"
+                                        fontWeight="bold">
+                                        {errorMsg}
+                                    </Text>
+                                </HStack>
+                                
+                            )}
+                        </Box>
+                        <Button 
+                            type="submit"
+                            width="100%" 
+                            rounded="0.8rem" 
+                            fontSize="1.2rem"
+                            fontWeight="semibold">Sign Up
+                        </Button>
                     </Box>
-                    <Button 
-                        type="submit"
-                        width="100%" 
-                        rounded="0.8rem" 
-                        fontSize="1.2rem"
-                        fontWeight="semibold">Sign Up</Button>
-                </Box>
+                </form>
             </Box>
             <Text
                 textAlign="center" 
