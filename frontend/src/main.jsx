9d4c2c2cac550a7a98b4fbb8f792ac8fb60d5b9b
AFanOfHololive <ios2004@icloud.com>
2026-03-02 15:22:11 +0700

    fix(frontend-pwa): added sw worker and web manifest for pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router';
-import router from './Router';
-// Chakra UI 
-import { ChakraProvider} from '@chakra-ui/react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
+import router from "./Router";
+// Chakra UI
+import { ChakraProvider } from "@chakra-ui/react";
 import { system } from "./theme";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ChakraProvider value={system}>               
-        <RouterProvider router={router} />
-    </ChakraProvider>
-  </StrictMode>,
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/sw.js")
+            .then((reg) => {
+                console.log("SW registered:", reg.scope);
+            })
+            .catch((err) => {
+                console.error("SW registration failed:", err);
+            });
+    });
+}
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ChakraProvider value={system}>
+            <RouterProvider router={router} />
+        </ChakraProvider>
+    </StrictMode>,
 );
- 
+
