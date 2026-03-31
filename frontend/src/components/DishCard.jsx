3f9ad4995ad74aa983e98b28575083e95d4adbe5
Luna Le <104323277@student.swin.edu.au>
2026-03-31 17:18:58 +0700

    Feat(frontend): Finish SearchPage

--- frontend/src/components/DishCard.jsx
+++ frontend/src/components/DishCard.jsx
@@ -19,30 +19,34 @@ export default function DishCard({dishImage = NO_IMAGE_PLACEHOLDER_URL, dishName
 
     return (
         <Card.Root 
-            w="sm" rounded="2xl" 
+            maxW="sm" overflow="hidden"
+            rounded="2xl" 
             boxShadow="4px 4px 12px rgba(0, 0, 0, 0.2)"
-
+            display="flex"
+            h="420px"
             // cursor="pointer" onClick={() => navigate('/app/dish/dish_name')}
         >
             <Image 
                 src = {`/assets/images/dishes/${assetNameProcess(dishName)}.webp`}
                 alt="A bowl of bun cha Hanoi"
-                borderRadius="md"
+                h="200px"  
+                w="100%"
+                objectFit="cover"
             />
-            <Card.Body>
-                <Card.Title>
+            <Card.Body pb={1}>
+                <Card.Title lineClamp={1} > 
                     {dishName}
                 </Card.Title>
 
-                <Card.Description>
+                <Card.Description
+                    lineClamp={4} mt={2}
+                >
                     {dishDescription}
                 </Card.Description>
             </Card.Body>
-            <Card.Footer>
+            <Card.Footer  mt="auto">
                 <Tag.Root>
-                    <Tag.Label>
-                        {dishCourse}
-                    </Tag.Label>
+                    <Tag.Label> {dishCourse} </Tag.Label>
                 </Tag.Root>
             </Card.Footer>
         </Card.Root>
